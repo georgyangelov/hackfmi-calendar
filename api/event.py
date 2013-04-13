95797a85afe7a8dfdfdd49f8e76e711cc05ff471
filareta <filareta_yordanova@abv.bg>
2013-04-14 00:49:30 +0300

    fix request methods

--- api/event.py
+++ api/event.py
@@ -17,14 +17,12 @@ class Event(Document):
     def to_json(self):
         representation = self.__dict__["_data"].copy()
         del representation[None]
-        del representation['password']
         return json.dumps(representation)
 
 
 @post('/events/session/:session_key')
 def create_events(session_key):
     event = Event()
-    print(event.__dict__["_data"])
     event.name = request.forms.getunicode('name')
     event.description = request.forms.getunicode('description')
     event.date = request.forms.getunicode('date')
@@ -32,7 +30,7 @@ def create_events(session_key):
         if session_key in user.session_keys:
             event.creator = user
         else:
-            error403("There is no user with that session key")
+            return error403("There is no user with that session key")
     event.tags = request.forms.getunicode('Tags')
     event.comments = request.forms.getunicode('Comment')
     event.users_approved.append(user)
@@ -43,13 +41,19 @@ def create_events(session_key):
 @get('/events/tags/:tag')
 def get_events(tag):
     events = Event.objects()
-    return [event.to_json() for event in events if tag in event.tags]
+    if events:
+        return [event.to_json() for event in events if tag in event.tags]
+    else:
+        return error403("There is not such tag")
 
 
 @get('/events/:event_id')
 def get_events_by_id(event_id):
     events = Event.objects()
-    return [event.to_json() for event in events if event_id in event.id_field]
+    if events:
+        return [event.to_json() for event in events if event_id == event.id_field]
+    else:
+        return error403("There is not such event")
 
 
 @get('/events/month/:month_id')
