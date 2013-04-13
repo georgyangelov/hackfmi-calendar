98123899b2cb336b16b5797ab81cfa4a16574318
filareta <filareta_yordanova@abv.bg>
2013-04-13 14:31:43 +0300

    change imported files

--- api/user.py
+++ api/user.py
@@ -1,9 +1,6 @@
 from mongoengine import *
 
 
-#connect('calendar', host='mongodb://hackfmi-calendar-user:kalinka@ds059947.mongolab.com:59947/hackfmi-calendar')
-
-
 class User(Document):
     first_name = StringField(required=True)
     last_name = StringField(required=True)
