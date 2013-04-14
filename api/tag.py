38657fd545e4a9b62493642241732a8ec96f074d
filareta <filareta_yordanova@abv.bg>
2013-04-14 06:11:30 +0300

    add tags

--- api/tag.py
+++ api/tag.py
@@ -1,7 +1,22 @@
 import datetime
 from mongoengine import *
-
+from bottle import *
+from api.user import *
 
 class Tag(Document):
-    name = StringField(required=True)
-    administrator = ReferenceField("User")
+    name = StringField(required=True, unique=True)
+    administrator = IntField()
+
+
+@post('/tags/:session_key')
+def add_tags(session_key):
+    tag = Tag()
+    tag.name = request.forms.getunicode('name')
+    users = User.objects()
+    admins = [user for user in users if session_key in user.session_keys]
+    if admins:
+        tag.administrator = admins[0].student_id
+    else:
+        return error403("There is no user with that session key")
+    tag.save()
+    return {"success": True}
